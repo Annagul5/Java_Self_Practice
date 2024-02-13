99e379c30dec84a4e69ecd77f6dac5ccf30387ae
annagul <annaberdiyeva55@gmail.com>
2024-02-14 00:33:24 +0300

    self practice pushing

--- src/javaDay12/CircleClients.java
+++ src/javaDay12/CircleClients.java
@@ -3,11 +3,12 @@ package javaDay12;
 public class CircleClients {
     public static void main(String[] args) {
 
-        Circle circle = new Circle(-3);
+        Circle circle = new Circle(3);
 
 
         System.out.println(circle.getRadius());
         System.out.println(circle.calcArea());
         System.out.println(circle.calcPerimeter());
+        System.out.println(circle);
     }
 }
--- src/javaDay12/RectangleClients.java
+++ src/javaDay12/RectangleClients.java
@@ -1,4 +1,17 @@
 package javaDay12;
 
 public class RectangleClients {
+
+    public static void main(String[] args) {
+
+    Restangle restangle = new Restangle(8,10);
+
+        System.out.println(restangle.getLength());
+        System.out.println(restangle.getWidth());
+
+        System.out.println(restangle.calcArea());
+        System.out.println(restangle.calcPerimeter());
+        System.out.println(restangle);
+
+    }
 }
